c600b11a94b3a795b31b98caba206239f07bc2d8
Jackson L. Bryant <jackbryant5589@gmail.com>
2025-04-17 12:31:45 -0400

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,18 +1,23 @@
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from './types/supabase'
 
-if (!(import.meta.env.VITE_SUPABASE_URL) || !(import.meta.env.VITE_SUPABASE_KEY)) {
+// Throw an error if the required environment variables are missing
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+
+if (!supabaseUrl || !supabaseKey) {
   throw new Error('❌ Missing Supabase environment variables!')
 }
 
+// Create the Supabase client instance
 const supabase = createClient<Database>(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY,
+  supabaseUrl,
+  supabaseKey,
   {
     global: {
       headers: {
-        apikey: import.meta.env.VITE_SUPABASE_KEY!,
-        Authorization: `Bearer ${import.meta.env.VITE_SUPABASE_KEY!}`,
+        apikey: supabaseKey,
+        Authorization: `Bearer ${supabaseKey}`,
       },
     },
     auth: {
